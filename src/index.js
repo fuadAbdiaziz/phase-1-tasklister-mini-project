b0e0d0ae91a8e4b4cf818118acfb1fd630ae3d26
Fuad <fuadabdaz99@gmail.com>
2023-03-26 11:10:38 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  function collectData(){
+    let form=document.querySelector("#new-task-description")
+
+    form.addEventListener("submit", (event) =>{
+      event.preventDefault()
+      let description=document.querySelector("#description")
+      let formData = {
+        TaskDescription:event.target.description.value,
+      }
+      console.log(formData)
+    })
+  }
+  
 });
+collectData()
